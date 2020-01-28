8c0c652a4fa48fc2187f80599663620f4d16ed79
Sergey S <birdflu@mail.ru>
2020-01-28 13:44:09 +0300

    Перегрузка конструкторов

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    private Solution(int a) {
+    }
+    public Solution(float a) {
+    }
+    protected Solution(String a) {
+    }
+    Solution(){
+
+    }
+}
+
