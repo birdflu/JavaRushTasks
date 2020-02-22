bbfbd7946f41eb3fb50ef3d02c75acb51107b85c
Sergey S <birdflu@mail.ru>
2020-02-22 08:00:53 +0300

    Контекстная реклама

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task19.task1927;
+
+/* 
+Контекстная реклама
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        //запоминаем настоящий PrintStream в специальную переменную
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String[] result = outputStream.toString().split(System.lineSeparator());
+
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+
+        //выводим с рекламой
+
+        int counter = 0;
+        for (String s: result
+             ) {
+            System.out.println(s);
+            counter++;
+            if (counter == 2) {System.out.println("JavaRush - курсы Java онлайн"); counter = 0;}
+        }
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
