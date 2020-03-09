c8d45ce266b095a16721eb707b517b47e9777f5b
Sergey S <birdflu@mail.ru>
2020-03-09 20:47:56 +0300

    Машину на СТО не повезем!

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            //init wheels here
+            wheels = new ArrayList<>();
+            for (String wheelValue : loadWheelNamesFromDB()){
+                Wheel wheel = Wheel.valueOf(wheelValue);
+                wheels.add(wheel);
+            }
+            if (wheels.size() != 4) throw new IllegalArgumentException();
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car.wheels);
+    }
+}
