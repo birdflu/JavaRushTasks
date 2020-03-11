1dd62cbb1d5ac76dafeccac9f373b16013bb060c
Sergey S <birdflu@mail.ru>
2020-03-11 14:27:21 +0300

    Живем своим умом

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task25.task2512;
+
+/*
+Живем своим умом
+*/
+public class Solution implements Thread.UncaughtExceptionHandler {
+  
+  @Override
+  public void uncaughtException(Thread t, Throwable e) {
+    
+    if (e != null) {
+      t.interrupt();
+      this.uncaughtException(t, e.getCause());
+      System.out.println(e.getClass().getName() + ": " + e.getMessage());
+    }
+  }
+  
+  public static void main(String[] args) {
+    new Solution().uncaughtException(Thread.currentThread(), new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI"))));
+  }
+}
