f4dfad653f539144a41044bab41117f6d3721d69
Sergey S <birdflu@mail.ru>
2020-03-30 22:38:14 +0300

    Add Client.SocketThread.clientHandshake() and Client.SocketThread.clientMainLoop

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -12,7 +12,7 @@ public class Client {
   private volatile boolean clientConnected = false;
   
   public class SocketThread extends Thread {
-    
+  
     protected void processIncomingMessage(String message) {
       ConsoleHelper.writeMessage(message);
     }
@@ -27,8 +27,35 @@ public class Client {
   
     protected void notifyConnectionStatusChanged(boolean clientConnected) {
       Client.this.clientConnected = clientConnected;
-      synchronized (Client.this){
+      synchronized (Client.this) {
         Client.this.notify();
+      }
+    }
+  
+    protected void clientHandshake() throws IOException, ClassNotFoundException {
+      while (true) {
+        Message message = connection.receive();
+        if (message.getType() == MessageType.NAME_REQUEST) {
+          String userName = getUserName();
+          connection.send(new Message(MessageType.USER_NAME, userName));
+        } else if (message.getType() == MessageType.NAME_ACCEPTED) {
+          notifyConnectionStatusChanged(true);
+          return;
+        } else throw new IOException("Unexpected MessageType");
+      }
+    }
+  
+    protected void clientMainLoop() throws IOException, ClassNotFoundException {
+      while (true) {
+        Message message = connection.receive();
+        MessageType type = message.getType();
+        if (type == MessageType.TEXT)
+          processIncomingMessage(message.getData());
+        else if (type == MessageType.USER_ADDED)
+          informAboutAddingNewUser(message.getData());
+        else if (type == MessageType.USER_REMOVED)
+          informAboutDeletingNewUser(message.getData());
+        else throw new IOException("Unexpected MessageType");
       }
     }
   }
