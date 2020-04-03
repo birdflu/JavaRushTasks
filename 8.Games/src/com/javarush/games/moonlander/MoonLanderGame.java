19793c49eb34575e887abc6c89b4198320deec21
Sergey S <birdflu@mail.ru>
2020-04-03 13:48:22 +0300

    Moon Lander (7/23). Landscape and rocket.move() are added

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -6,15 +6,18 @@ public class MoonLanderGame extends Game {
   public static final int WIDTH = 64;
   public static final int HEIGHT = 64;
   private Rocket rocket;
+  private GameObject landscape;
   
   @Override
   public void initialize() {
     setScreenSize(WIDTH, HEIGHT);
     createGame();
+    showGrid(false);
   }
   
   private void createGame() {
-    rocket = new Rocket(WIDTH/2, 0);
+    createGameObjects();
+    setTurnTimer(50);
     drawScene();
   }
   
@@ -24,7 +27,25 @@ public class MoonLanderGame extends Game {
         setCellColor(x, y, Color.BLACK);
       }
     }
+    landscape.draw(this);
     rocket.draw(this);
     
+  }
+  
+  private void createGameObjects() {
+    landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
+    rocket  = new Rocket(WIDTH/2, 0);
+  }
+  
+  @Override
+  public void onTurn(int step) {
+    rocket.move();
+    drawScene();
+  }
+  
+  @Override
+  public void setCellColor(int x, int y, Color color) {
+    if (x > 0 && y > 0 && x < WIDTH && y < HEIGHT)
+      super.setCellColor(x, y, color);
   }
 }
