42896e6fae1138e0950209c4bc313d1a82be5715
Sergey S <birdflu@mail.ru>
2020-04-04 20:38:41 +0300

    Moon Lander (1/23). Rocket.move and Rocket.checkBorders are added

--- 8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -1,6 +1,7 @@
 package com.javarush.games.moonlander;
 
 public class Rocket extends GameObject {
+  private double speedX = 0;
   private double speedY = 0;
   private double boost = 0.05;
   
@@ -8,8 +9,16 @@ public class Rocket extends GameObject {
     super(x, y, ShapeMatrix.ROCKET);
   }
   
-  public void move() {
-    speedY += boost;
-    y += speedY;
+  public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
+    if (isUpPressed) { speedY -= boost; y += speedY; } else { speedY += boost; y += speedY; }
+    if (isLeftPressed) { speedX -= boost; x += speedX; }
+    if (isRightPressed) { speedX += boost; x += speedX; }
+    checkBorders();
+  }
+  
+  private void checkBorders() {
+    if (x < 0) { x = 0; speedX = 0; }
+    if (x + width > MoonLanderGame.WIDTH) { x = MoonLanderGame.WIDTH - width; speedX = 0; }
+    if (y < 0)  { y = 0; speedY = 0; }
   }
 }
