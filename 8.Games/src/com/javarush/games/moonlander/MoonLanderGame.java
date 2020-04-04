363789f913a344529d1634be3ddd89115ac2e0d3
Sergey S <birdflu@mail.ru>
2020-04-04 20:16:30 +0300

    Moon Lander (9/23). onKeyPress и onKeyReleased are added

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -1,12 +1,17 @@
 package com.javarush.games.moonlander;
 
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+import com.javarush.engine.cell.Key;
 
 public class MoonLanderGame extends Game {
   public static final int WIDTH = 64;
   public static final int HEIGHT = 64;
   private Rocket rocket;
   private GameObject landscape;
+  private boolean isUpPressed;
+  private boolean isLeftPressed;
+  private boolean isRightPressed;
   
   @Override
   public void initialize() {
@@ -18,6 +23,9 @@ public class MoonLanderGame extends Game {
   private void createGame() {
     createGameObjects();
     setTurnTimer(50);
+    isLeftPressed = false;
+    isRightPressed = false;
+    isUpPressed = false;
     drawScene();
   }
   
@@ -46,5 +54,19 @@ public class MoonLanderGame extends Game {
   public void setCellColor(int x, int y, Color color) {
     if (x > 0 && y > 0 && x < WIDTH && y < HEIGHT)
       super.setCellColor(x, y, color);
+  }
+  
+  @Override
+  public void onKeyPress(Key key) {
+    if (key == Key.LEFT) { isLeftPressed = true; isRightPressed = false; }
+    if (key == Key.RIGHT) {isRightPressed = true; isLeftPressed = false; }
+    if (key == Key.UP) {isUpPressed = true; };
+  }
+  
+  @Override
+  public void onKeyReleased(Key key) {
+    if (key == Key.LEFT) { isLeftPressed = false; }
+    if (key == Key.RIGHT) {isRightPressed = false; }
+    if (key == Key.UP) {isUpPressed = false; };
   }
 }
