d700ca7c5847504a1286533a1d5218afcad07a9e
Sergey S <birdflu@mail.ru>
2020-04-08 23:13:48 +0300

    Archiver. Command and ExitCommand are added.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
@@ -13,5 +15,7 @@ public class Archiver {
     System.out.println("Input path for archiving: ");
     String archiveSource = reader.readLine();
     zipFileManager.createZip(Paths.get(archiveSource));
+    ExitCommand exitCommand = new ExitCommand();
+    exitCommand.execute();
   }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -5,27 +5,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleHelper {
-  private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-  
-  public static void writeMessage(String message) {
-    System.out.println(message);
-  }
-  
-  public static String readString() {
-    while (true)
-      try {
-        return bufferedReader.readLine();
-      } catch (IOException e) {
-        System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
-      }
-  }
-  
-  public static int readInt() {
-    while (true)
-      try {
-        return Integer.parseInt(readString());
-      } catch (NumberFormatException e) {
-        System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
-      }
-  }
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        String text = bis.readLine();
+        return text;
+    }
+
+    public static int readInt() throws IOException {
+        String text = readString();
+        return Integer.parseInt(text.trim());
+    }
 }
