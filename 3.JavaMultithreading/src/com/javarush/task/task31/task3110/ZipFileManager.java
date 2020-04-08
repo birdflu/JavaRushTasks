f4c62ded6a77bd7f33fbbb2c8c7d45c28c258172
Sergey S <birdflu@mail.ru>
2020-04-08 23:00:26 +0300

    createZip

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
   private Path zipFile;
@@ -11,5 +15,17 @@ public class ZipFileManager {
   
   public void createZip(Path source) throws Exception {
   
+    try (ZipOutputStream outputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+         InputStream inputStream = Files.newInputStream(source);
+    ) {
+      ZipEntry entry = new ZipEntry(source.getFileName().toString());
+      outputStream.putNextEntry(entry);
+      byte[] buffer = new byte[1000];
+      while (inputStream.available() > 0) {
+        int count = inputStream.read(buffer);
+        outputStream.write(buffer, 0, count);
+      }
+      outputStream.closeEntry();
+    }
   }
 }
