e3805a6b2f5cac3b5986573b9751e4f8c3b4f145
Sergey S <birdflu@mail.ru>
2020-04-08 23:20:05 +0300

    Archiver. Empty command classes are added.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ExitCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ExitCommand.java
@@ -3,8 +3,8 @@ package com.javarush.task.task31.task3110.command;
 import com.javarush.task.task31.task3110.ConsoleHelper;
 
 public class ExitCommand implements Command {
-  @Override
-  public void execute() throws Exception {
-    ConsoleHelper.writeMessage("До встречи!");
-  }
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("До встречи!");
+    }
 }
