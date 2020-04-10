825d653d0eba56f65fc9e3825bbfdace8dbf3166
Sergey S <birdflu@mail.ru>
2020-04-10 21:46:11 +0300

    Archiver. Refactoring FileManager.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -4,37 +4,39 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
-  private Path rootPath;
-  private List<Path> fileList;
-  
-  public FileManager(Path rootPath) throws IOException {
-    this.rootPath = rootPath;
-    this.fileList = new ArrayList<>();
-    collectFileList(rootPath);
-  }
-  
-  public List<Path> getFileList() {
-    return fileList;
-  }
-  
-  private void collectFileList(Path path) throws IOException {
-    if (Files.isRegularFile(path)) {
-      String absolutePath = path.toAbsolutePath().toString();
-      String absoluteRoot = rootPath.toAbsolutePath().toString();
-      String relativePath = absolutePath.replace(absoluteRoot, "").substring(1);
-      fileList.add(Paths.get(relativePath));
-      } else if (Files.isDirectory(path)) {
-      DirectoryStream stream = Files.newDirectoryStream(path);
-      for (Object filePath: stream
-           ) {
-        collectFileList((Path) filePath);
-      }
-      stream.close();
+    private Path rootPath;
+    private List<Path> fileList;
+
+    public FileManager(Path rootPath) throws IOException {
+        this.rootPath = rootPath;
+        this.fileList = new ArrayList<>();
+        collectFileList(rootPath);
     }
-  }
+
+    public List<Path> getFileList() {
+        return fileList;
+    }
+
+    private void collectFileList(Path path) throws IOException {
+        // Добавляем только файлы
+        if (Files.isRegularFile(path)) {
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
+        }
+
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+                for (Path file : directoryStream) {
+                    collectFileList(file);
+                }
+            }
+        }
+    }
 }
