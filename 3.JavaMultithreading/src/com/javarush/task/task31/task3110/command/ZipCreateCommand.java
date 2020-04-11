dcb09b1117c7b6fe04a8fa8572fc38660fe60b81
Sergey S <birdflu@mail.ru>
2020-04-11 11:44:35 +0300

    Archiver. FileProperties is added.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -8,19 +8,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ZipCreateCommand extends ZipCommand {
-  @Override
-  public void execute() throws Exception {
-    try {
-      ConsoleHelper.writeMessage( "Создание архива." );
-      ZipFileManager zipFileManager = getZipFileManager();
-      ConsoleHelper.writeMessage( "Введите полное имя файла или директории для архивации" );
-      Path path = Paths.get(ConsoleHelper.readString());
-      zipFileManager.createZip(path);
-      ConsoleHelper.writeMessage("Архив создан.");
-    } catch (PathIsNotFoundException e) {
-      ConsoleHelper.writeMessage( "Вы неверно указали имя файла или директории.");
+    @Override
+    public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(sourcePath);
+
+            ConsoleHelper.writeMessage("Архив создан.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
-    
-  }
-  
 }
