d211cf4ed5440a9dfc0d26ddc19e86a73390f2ee
Sergey S <birdflu@mail.ru>
2020-04-11 10:12:38 +0300

    Racer. PlayerCar is added

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -7,6 +7,8 @@ public class RacerGame extends Game {
   public static final int HEIGHT = 64;
   public static final int CENTER_X = WIDTH / 2;
   public static final int ROADSIDE_WIDTH = 14;
+  private RoadMarking roadMarking;
+  private PlayerCar player;
   
   @Override
   public void initialize() {
@@ -16,12 +18,16 @@ public class RacerGame extends Game {
   }
   
   private void createGame() {
+    roadMarking = new RoadMarking();
+    player = new PlayerCar();
     drawScene();
     
   }
   
   private void drawScene() {
     drawField();
+    roadMarking.draw(this);
+    player.draw(this);
   }
   
   
