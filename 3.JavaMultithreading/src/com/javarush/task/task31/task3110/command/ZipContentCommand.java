93ec4aa5e5d3a662441321e003a853ae72c40154
Sergey S <birdflu@mail.ru>
2020-04-12 21:19:43 +0300

    Archiver. ZipFileManager.extractAll is added.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -7,12 +7,19 @@ import com.javarush.task.task31.task3110.ZipFileManager;
 import java.util.List;
 
 public class ZipContentCommand extends ZipCommand {
-  @Override
-  public void execute() throws Exception {
-    ConsoleHelper.writeMessage("Содержимое архива:");
-    ZipFileManager zipFileManager = getZipFileManager();
-    List<FileProperties> filesList = zipFileManager.getFilesList();
-    filesList.forEach(fileProperties -> System.out.println(fileProperties));
-    ConsoleHelper.writeMessage( "Содержимое архива прочитано.");
-  }
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
+        }
+
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
+    }
 }
