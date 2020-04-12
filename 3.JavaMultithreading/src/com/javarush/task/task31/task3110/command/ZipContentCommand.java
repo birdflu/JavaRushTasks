e257d1c528e154e8c5bb5af3a80685d2290d8015
Sergey S <birdflu@mail.ru>
2020-04-12 17:37:19 +0300

    Archiver. ZipContentCommand.execute() is added.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,8 +1,18 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
   @Override
   public void execute() throws Exception {
-  
+    ConsoleHelper.writeMessage("Содержимое архива:");
+    ZipFileManager zipFileManager = getZipFileManager();
+    List<FileProperties> filesList = zipFileManager.getFilesList();
+    filesList.forEach(fileProperties -> System.out.println(fileProperties));
+    ConsoleHelper.writeMessage( "Содержимое архива прочитано.");
   }
 }
