d963faccebcda60d99eb18b0c4c247e641bf6435
Sergey S <birdflu@mail.ru>
2020-04-12 22:30:07 +0300

    Archiver. ZipFileManager.removeFiles is added.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipExtractCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipExtractCommand.java
@@ -8,20 +8,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ZipExtractCommand extends ZipCommand {
-  @Override
-  public void execute() throws Exception {
-    try {
-      ConsoleHelper.writeMessage("Распаковка архива.");
-      
-      ZipFileManager zipFileManager = getZipFileManager();
-      
-      ConsoleHelper.writeMessage("Введите полный путь к папке для распаковки:");
-      Path sourcePath = Paths.get(ConsoleHelper.readString());
-      zipFileManager.extractAll(sourcePath);
-      
-      ConsoleHelper.writeMessage("Архив распакован.");
-      
-    } catch (PathIsNotFoundException e) {
-      ConsoleHelper.writeMessage("Вы неверно указали путь к архиву.");
+    @Override
+    public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива.");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите путь для распаковки:");
+            Path destinationPath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(destinationPath);
+
+            ConsoleHelper.writeMessage("Архив был распакован.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Неверный путь для распаковки.");
+        }
     }
-  }}
+}
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipRemoveCommand.java
@@ -1,8 +1,22 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipRemoveCommand extends ZipCommand {
   @Override
   public void execute() throws Exception {
-  
+      ConsoleHelper.writeMessage("Удаление из архива.");
+    
+      ZipFileManager zipFileManager = getZipFileManager();
+    
+      ConsoleHelper.writeMessage("Введите полное имя файла или директории для удаления:");
+      Path sourcePath = Paths.get(ConsoleHelper.readString());
+      zipFileManager.removeFile(sourcePath);
+    
+      ConsoleHelper.writeMessage("Архив создан.");
+    
   }
 }
