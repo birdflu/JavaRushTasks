b55d541d820ee875da03bc56a5df175e6d44a7a4
Sergey S <birdflu@mail.ru>
2020-04-18 19:35:31 +0300

    HTML Editor (20)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -19,7 +19,7 @@ public class Controller {
   }
   
   public void init() {
-  
+    createNewDocument();
   }
   
   public void exit() {
@@ -68,6 +68,11 @@ public class Controller {
   }
   
   public void createNewDocument() {
+    view.selectHtmlTab();
+    resetDocument();
+    view.setTitle("HTML редактор");
+    view.resetUndo();
+    currentFile = null;
   }
   
   public void openDocument() {
