6baf08d30dc17181d90eed55965e1dc5b5820465
Sergey S <birdflu@mail.ru>
2020-04-18 21:32:06 +0300

    HTML Editor (22)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,13 +1,11 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.*;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 public class Controller {
   private View view;
@@ -20,10 +18,6 @@ public class Controller {
   
   public void init() {
     createNewDocument();
-    String name = "dsfd.sf.hTmL";
-    System.out.println(name.matches(".*html$"));
-    System.out.println(name.matches("(?i:.*\\." +"html" +"$)"));
-    
   }
   
   public void exit() {
@@ -83,8 +77,22 @@ public class Controller {
   }
   
   public void saveDocument() {
+  
   }
   
   public void saveDocumentAs() {
+    view.selectHtmlTab();
+    JFileChooser fileChooser = new JFileChooser();
+    if (JFileChooser.APPROVE_OPTION == fileChooser.showSaveDialog(view)) {
+      fileChooser.setFileFilter(new HTMLFileFilter());
+      currentFile = fileChooser.getSelectedFile();
+      view.setTitle(currentFile.getName());
+      try {
+        FileWriter writer = new FileWriter(currentFile);
+        new HTMLEditorKit().write(writer,document,0,document.getLength());
+      } catch (IOException | BadLocationException e) {
+        ExceptionHandler.log(e);
+      }
+    }
   }
 }
