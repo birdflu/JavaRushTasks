73266bcee3b1764474ec0b940de56280901e49db
Sergey S <birdflu@mail.ru>
2020-04-22 10:44:57 +0300

    AmigoSet (2)

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,13 +1,25 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable, Set {
+  private static final Object PRESENT = new Object();
+  private transient HashMap<E, Object> map;
+  
+  public AmigoSet() {
+    map = new HashMap<>();
+  }
+  
+  public AmigoSet(Collection<? extends E> collection) {
+    map = new HashMap<>(16 > collection.size() / .75f
+            ? 16
+            : (int) (collection.size() / .75f + 1));
+    this.addAll(collection);
+  }
+  
   @Override
   public Iterator iterator() {
     return null;
@@ -28,5 +40,13 @@ public class AmigoSet<E> extends AbstractSet implements Serializable, Cloneable,
     return false;
   }
   
-
+  @Override
+  public boolean add(Object o) {
+    try {
+      return map.put((E) o, PRESENT) == null;
+    } catch (Exception e) {
+      return false;
+    }
+  
+  }
 }
