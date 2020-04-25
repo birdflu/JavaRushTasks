ff41a7e168d19be3c898e5e73f4ce4a04b7f7541
Sergey S <birdflu@mail.ru>
2020-04-25 19:40:11 +0300

    Разбираемся в красно-черном дереве

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3604/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task36.task3604;
+
+/* 
+Разбираемся в красно-черном дереве
+*/
+public class Solution {
+    public static void main(String[] args) {
+        RedBlackTree rbt = new RedBlackTree();
+        rbt.insert(10);
+        rbt.insert(11);
+        System.out.println(rbt.toString());
+    
+    
+    }
+    
+    
+}
