89e9f1a1e369037908d663380cffe6fb13fbd898
Sergey S <birdflu@mail.ru>
2020-04-28 13:33:58 +0300

    Shortener (3)

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,12 +1,26 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 public class Shortener {
-  public Long getId(String string) {
-    return null;
+  private Long lastId = 0L; // последнее значение идентификатора, которое было использовано при добавлении новой строки в хранилище.
+  private StorageStrategy storageStrategy; // стратегия хранения данных
+  
+  public Shortener(StorageStrategy storageStrategy) {
+    this.storageStrategy = storageStrategy;
   }
   
-  public String getString(Long id) {
-    return null;
+  public synchronized Long getId(String string) {
+    if (storageStrategy.containsValue(string))
+      return storageStrategy.getKey(string);
+    else {
+      storageStrategy.put(++lastId, string);
+    }
+    return lastId;
+  }
+  
+  public synchronized String getString(Long id) {
+    return storageStrategy.getValue(id);
   }
   
   
