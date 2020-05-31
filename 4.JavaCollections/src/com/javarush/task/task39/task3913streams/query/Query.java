6b4de020d8da736b66c4c3112d64bb08f3112dcd
Sergey S <birdflu@mail.ru>
2020-05-31 10:49:43 +0300

    Query refactoring

--- 4.JavaCollections/src/com/javarush/task/task39/task3913streams/query/Query.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913streams/query/Query.java
@@ -5,67 +5,88 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Query {
-  private String fieldName;
+  private final String fieldName;
   private String conditionKey = null;
   private String conditionValue = null;
   private Date conditionStartDate = null;
   private Date conditionEndDate = null;
   
-  public Query(String query) {
-    // query = "get ip for user = \"Eduard Petrovich Morozko\" and date between \"11.12.2013 0:00:00\" and \"03.01.2014 23:59:59\"";
-    int get_BeginIndex = query.indexOf("get ");
-    int fieldBeginIndex = get_BeginIndex + "get ".length();
-    int _for_BeginIndex = query.indexOf(" for ");
-    int fieldEndIndex = -1;
-    int keyBeginIndex = -1;
-    int firstBraceIndex = -1;
-    int keyEndIndex = -1;
-    int secondBraceIndex = -1;
-    int thirdBraceIndex = -1;
-    int forthBraceIndex = -1;
-    int fifthBraceIndex = -1;
-    int sixthBraceIndex = -1;
-  
-    if (_for_BeginIndex == -1) {
-      fieldEndIndex = query.length();
-    } else {
-      fieldEndIndex = _for_BeginIndex;
-      keyBeginIndex = _for_BeginIndex + " for ".length();
-      firstBraceIndex = query.indexOf("\"");
-      keyEndIndex = firstBraceIndex - " = \"".length() + 1;
-      secondBraceIndex = query.indexOf("\"", firstBraceIndex + 1);
-      thirdBraceIndex = query.indexOf("\"", secondBraceIndex + 1);
-      if (thirdBraceIndex > 0) {
-        forthBraceIndex = query.indexOf("\"", thirdBraceIndex + 1);
-        fifthBraceIndex = query.indexOf("\"", forthBraceIndex + 1);
-        sixthBraceIndex = query.indexOf("\"", fifthBraceIndex + 1);
-      }
+  private static class Element {
+    int startIndex;
+    int endIndex;
+    
+    public Element(int startIndex, int endIndex) {
+      this.startIndex = startIndex;
+      this.endIndex = endIndex;
     }
-  
-    fieldName = query.substring(fieldBeginIndex, fieldEndIndex);
-    if (keyBeginIndex > 0) conditionKey = query.substring(keyBeginIndex, keyEndIndex);
-    if (firstBraceIndex > 0) conditionValue = query.substring(firstBraceIndex + 1, secondBraceIndex);
-    try {
-      if (thirdBraceIndex > 0) {
-        Date startDate = new SimpleDateFormat("d.M.yyyy H:m:s").parse(query.substring(thirdBraceIndex + 1, forthBraceIndex));
-        //!!! between тут такой - даты after и before не должны попадать в результат
-        long d = startDate.getTime();
-        d = d + 500L;
-        conditionStartDate = new Date(d);
-      }
-      if (fifthBraceIndex > 0) {
-        Date endDate = new SimpleDateFormat("d.M.yyyy H:m:s").parse(query.substring(fifthBraceIndex + 1, sixthBraceIndex));
-        long d = endDate.getTime();
-        d = d - 500L;
-        conditionEndDate = new Date(d);
-      }
-    } catch (ParseException e) {
-      e.printStackTrace();
+    
+    public Element() {
+      this.startIndex = -1;
+      this.endIndex = -1;
     }
-  // Query{fieldName='ip', conditionKey='event', conditionValue='SOLVE_TASK',
-    // conditionStartDate=Thu Jan 30 12:56:22 MSK 2014, conditionEndDate=Thu Oct 21 19:45:25 MSK 2021}
-  
   }
+  
+  private static class OffsetElements {
+    private final String query;
+    private final Element field;
+    private Element key = new Element();
+    private Element value = new Element();
+    private Element data1 = new Element();
+    private Element data2 = new Element();
+    
+    public OffsetElements(String query) {
+      // query = "get ip for user = \"Eduard Petrovich Morozko\" and date between \"11.12.2013 0:00:00\" and \"03.01.2014 23:59:59\"";
+      this.query = query;
+      Element getStatement = new Element(query.indexOf("get "), query.indexOf("get ") + "get ".length() - 1);
+      Element forStatement = new Element(query.indexOf(" for "), -1);
+      
+      if (forStatement.startIndex == -1) {
+        field = new Element(getStatement.endIndex + 1, query.length());
+      } else {
+        field = new Element(getStatement.endIndex + 1, forStatement.startIndex);
+        value = new Element(query.indexOf("\""), getSlashIndex(query.indexOf("\"")));
+        key = new Element(forStatement.startIndex + " for ".length(), value.startIndex - " = \"".length() + 1);
+        
+        data1 = new Element(getSlashIndex(value.endIndex), -1);
+        if (data1.startIndex > 0) {
+          data1.endIndex = getSlashIndex(data1.startIndex);
+          data2 = new Element(getSlashIndex(data1.endIndex),
+                  getSlashIndex(getSlashIndex(data1.endIndex)));
+        }
+      }
+    }
+    
+    private int getSlashIndex(int startIndex) {
+      return query.indexOf("\"", startIndex + 1);
+    }
+  }
+  
+  public Query(String query) {
+    OffsetElements e = new OffsetElements(query);
+    
+    fieldName = query.substring(e.field.startIndex, e.field.endIndex);
+    if (e.key.startIndex > 0) conditionKey = query.substring(e.key.startIndex, e.key.endIndex);
+    if (e.value.startIndex > 0) conditionValue = query.substring(e.value.startIndex + 1, e.value.endIndex);
+    try {
+      //!!! between тут такой - даты after и before не должны попадать в результат
+      long delta = 500L;
+      if (e.data1.startIndex > 0) {
+        Date startDate = parseDate(query, e.data1.startIndex + 1, e.data1.endIndex);
+        conditionStartDate = new Date(startDate.getTime() + delta);
+      }
+      if (e.data2.startIndex > 0) {
+        Date endDate = parseDate(query, e.data2.startIndex + 1, e.data2.endIndex);
+        conditionEndDate = new Date(endDate.getTime() - delta);
+      }
+    } catch (ParseException exception) {
+      exception.printStackTrace();
+    }
+  }
+  
+  private Date parseDate(String query, int from, int to) throws ParseException {
+    return new SimpleDateFormat("d.M.yyyy H:m:s").parse(query.substring(from, to));
+  }
+  
   
   public String getFieldName() {
     return fieldName;
