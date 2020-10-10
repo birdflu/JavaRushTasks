15c1f1034a6bf2d825e467832822dcda7d033891
birdflu <birdflu@mail.ru>
2020-10-10 14:19:47 +0300

    Ресторан(9)Л

--- 12.MyStudyHall/src/studyhall/permutation/Permutation.java
+++ 12.MyStudyHall/src/studyhall/permutation/Permutation.java
@@ -13,7 +13,18 @@ public class Permutation {
 
     public static void main(String[] args) {
         Permutation solution = new Permutation();
-        System.out.println(solution.permutate(
+//        System.out.println(solution.permutate(
+//                new ArrayList<>() {{
+//                    add(new ArrayList<>() {{
+//                    }});
+//                    add(new ArrayList<>() {{
+//                        add(1);
+//                        add(2);
+//                        add(3);
+//                    }});
+//                }}));
+
+        System.out.println(solution.permutation(
                 new ArrayList<>() {{
                     add(new ArrayList<>() {{
                     }});
@@ -59,19 +70,19 @@ public class Permutation {
 //               (perm result tail))))
 
     public List permutate(List list) {
-        System.out.println("  : " +   list);
+//        System.out.println("  : " +   list);
         List result = (List) list.get(0);
         List tail = (List) list.get(1);
-        System.out.println("=>" + result + " " + tail + " => "  + perm(result, tail));
+//        System.out.println("=>" + result + " " + tail + " => "  + perm(result, tail));
 //        System.out.println("=>" + result + " " + tail );
 
         return perm(result, tail).stream()
                 .map((x) -> {
                     List t = new ArrayList(tail);
                     t.removeAll((List) x.get(0));
-                    System.out.println("t=" + t);
-                    System.out.println("x.get(0)=" + x.get(0));
-                    System.out.println("x=" + x);
+//                    System.out.println("t=" + t);
+//                    System.out.println("x.get(0)=" + x.get(0));
+//                    System.out.println("x=" + x);
                     //((List)x.get(0)).addAll(new ArrayList(){{ add(t); }});
                     x.addAll(new ArrayList(){{ add(t); }});
                     //return x.get(0);
@@ -83,4 +94,20 @@ public class Permutation {
 //        return perm (new ArrayList<>() {{ add(2); }}, new ArrayList<>() {{ add(1); add(3); }});
     }
 
+//    (define (permutation items)
+//        (if (null? items)
+//            '()
+//            (append items (permutation (flatmap permutate items)))))
+
+    public List permutation (List list) {
+        if (list.isEmpty()) {
+            return list;
+        } else {
+            System.out.println("list " + list);
+            System.out.println("permutate(list " + permutate(list));
+            return (List) ((List<List>) permutate(list)).stream()
+                    .flatMap((x) -> (permutate(x)).stream())
+                    .collect(Collectors.toList());
+        }
+    }
 }
