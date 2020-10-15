b5a3da17b6fd7687cb98a42a78bf5e1f149eb789
birdflu <birdflu@mail.ru>
2020-10-15 17:41:08 +0300

    Advertisement comparators

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementComparator.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementComparator.java
@@ -1,27 +1,20 @@
 package com.javarush.task.task27.task2712.ad;
 
 import java.util.Comparator;
-import java.util.List;
 
-public class AdvertisementComparator implements Comparator<List> {
+public class AdvertisementComparator implements Comparator<Advertisement> {
 
-//    1. сумма денег, полученная от показов, должна быть максимальной из всех возможных вариантов
-//    4. если существует несколько вариантов набора видео-роликов с одинаковой суммой денег, полученной от показов, то:
-//    4.1. выбрать тот вариант, у которого суммарное время максимальное;
-//    4.2. если суммарное время у этих вариантов одинаковое, то выбрать вариант с минимальным количеством роликов;
+    // 2.4. Отобразить все рекламные ролики, отобранные для показа, в порядке уменьшения стоимости показа
+    // одного рекламного ролика в копейках. Вторичная сортировка - по увеличению стоимости показа
+    // одной секунды рекламного ролика в тысячных частях копейки.
 
     @Override
-    public int compare(List l1, List l2) {
-        Advertisement o1 = (Advertisement) l1.get(0);
-        Advertisement o2 = (Advertisement) l2.get(0);
-        if (o1.getAmountPerOneDisplaying() > o2.getAmountPerOneDisplaying())
-            return 1;
-        else if (o1.getAmountPerOneDisplaying() < o2.getAmountPerOneDisplaying())
-            return -1;
-        else if (o1.getDuration() > o2.getDuration())
-            return 1;
-        else if (o1.getDuration() < o2.getDuration())
-            return -1;
-        else return l2.size()-l1.size();
+    public int compare(Advertisement o1, Advertisement o2) {
+        int l = (int) (o2.getAmountPerOneDisplaying() - o1.getAmountPerOneDisplaying());
+        if (l != 0) return l;
+        else {
+            int p = (int) (o1.getPricePerSecond() - o2.getAmountPerOneDisplaying());
+            return p;
+        }
     }
 }
