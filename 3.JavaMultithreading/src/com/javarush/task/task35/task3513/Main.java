d145c2446a94b5a7e607d1336ac246511a974a7c
birdflu <birdflu@mail.ru>
2020-10-28 14:24:51 +0300

    2018 (5)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -2,6 +2,25 @@ package com.javarush.task.task35.task3513;
 
 public class Main {
   public static void main(String[] args) {
+/*    Tile[] tiles = {
+            new Tile(4),
+            new Tile(2),
+            new Tile(2),
+            new Tile(4),
+            new Tile(4),
+            new Tile(4),
+            new Tile(0)};
 
+    Model m = new Model();
+    m.compressTiles(tiles);
+    for (Tile tile : tiles) {
+      System.out.print(tile.value + " ");
+    }
+    System.out.println();
+    m.mergeTiles(tiles);
+    for (Tile tile : tiles) {
+      System.out.print(tile.value + " ");
+    }*/
   }
+
 }
--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -8,6 +8,8 @@ import java.util.stream.Stream;
 public class Model {
   private static final int FIELD_WIDTH = 4;
   private Tile[][] gameTiles;
+  protected int score = 0;
+  protected int maxTile = 0;
 
   public Model() {
     resetGameTiles();
@@ -39,6 +41,37 @@ public class Model {
 
   private static Stream<Object> flatten(Object[] array) {
     return Arrays.stream(array)
-            .flatMap(o -> o instanceof Object[]? flatten((Object[])o): Stream.of(o));
+            .flatMap(o -> o instanceof Object[] ? flatten((Object[]) o) : Stream.of(o));
   }
+
+  private void compressTiles(Tile[] tiles) {
+    int positiveCount = 0;
+    for (int i = tiles.length - 1; i >= 0; i--) {
+      if (tiles[i].value != 0) positiveCount++;
+      if (tiles[i].value == 0 && positiveCount > 0) {
+        Tile t = tiles[i + 1];
+        tiles[i + 1] = tiles[i];
+        tiles[i] = t;
+        positiveCount--;
+        i = i + 2;
+      }
+    }
+  }
+
+  private void mergeTiles(Tile[] tiles) {
+    for (int i = 0; i < tiles.length - 1; i++) {
+      if (tiles[i].value == 0) break;
+      if (tiles[i].value == tiles[i + 1].value) {
+        int newTileValue = tiles[i].value * 2;
+        tiles[i].value = newTileValue;
+        if (maxTile < newTileValue) maxTile = tiles[i].value;
+        score = score + newTileValue;
+        for (int j = i + 1; j < tiles.length - 1; j++) {
+          tiles[j] = tiles[j + 1];
+        }
+        tiles[tiles.length - 1] = new Tile();
+      }
+    }
+  }
+
 }
