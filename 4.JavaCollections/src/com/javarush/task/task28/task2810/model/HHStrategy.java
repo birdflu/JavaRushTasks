59b34907f049a009de9f705af2fc01572a5c1e90
birdflu <birdflu@mail.ru>
2020-10-30 13:00:30 +0300

    Aggregator (3)

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task28.task2810.model;
+
+/** Класс будет реализовывать конкретную стратегию работы с сайтом http://hh.ua/ и http://hh.ru/.*/
+public class HHStrategy implements Strategy {
+}
