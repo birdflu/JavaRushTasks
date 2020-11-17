f2b0d0c0bb6e29fa64e1ee9382f41b7ebdc022bd
birdflu <birdflu@mail.ru>
2020-11-18 00:08:00 +0300

    Buon Compleanno!

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4009/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task40.task4009;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+/* 
+Buon Compleanno!
+*/
+
+public class Solution {
+  public static void main(String[] args) {
+    System.out.println(getWeekdayOfBirthday("30.05.1984", "2015"));
+  }
+
+  public static String getWeekdayOfBirthday(String birthday, String year) {
+    //напишите тут ваш код
+    String s = null;
+
+    DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("d.M.yyyy");
+    LocalDate localDate = LocalDate.parse(birthday, dateFormatter);
+
+    Year yearNow = Year.parse(year);
+
+    dateFormatter = DateTimeFormatter.ofPattern("EEEE", Locale.ITALIAN);
+    localDate = LocalDate.of(yearNow.getValue(), localDate.getMonth(), localDate.getDayOfMonth());
+
+    s = localDate.format(dateFormatter);
+    return s;
+  }
+}
