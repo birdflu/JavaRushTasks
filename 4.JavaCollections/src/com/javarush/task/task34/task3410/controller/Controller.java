c55c8e207cf41d569e9b9516df7429688d8c667f
birdflu <birdflu@mail.ru>
2020-11-18 19:36:22 +0300

    Sokoban (1)

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task34.task3410.controller;
+
+import com.javarush.task.task34.task3410.model.Model;
+import com.javarush.task.task34.task3410.view.View;
+
+public class Controller {
+  Model model;
+  View view;
+
+  public Controller() {
+    this.model = new Model();
+    this.view = new View(this);
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
