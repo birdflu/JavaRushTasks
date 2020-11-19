b76ac1d1a7f3ef70c90131a8d1fdc3a8bc85d340
birdflu <birdflu@mail.ru>
2020-11-19 19:02:04 +0300

    Sokoban (13)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -5,7 +5,7 @@ import com.javarush.task.task34.task3410.model.GameObjects;
 import com.javarush.task.task34.task3410.model.Model;
 import com.javarush.task.task34.task3410.view.View;
 
-public class Controller implements EventListener{
+public class Controller implements EventListener {
   Model model;
   View view;
 
@@ -14,6 +14,8 @@ public class Controller implements EventListener{
     view = new View(this);
     view.init();
     model.restart();
+    model.setEventListener(this);
+    view.setEventListener(this);
   }
 
   public static void main(String[] args) {
@@ -27,21 +29,25 @@ public class Controller implements EventListener{
 
   @Override
   public void move(Direction direction) {
-
+    model.move(direction);
+    view.update();
   }
 
   @Override
   public void restart() {
-
+    model.restart();
+    view.update();
   }
 
   @Override
   public void startNextLevel() {
-
+    model.startNextLevel();
+    view.update();
   }
 
   @Override
   public void levelCompleted(int level) {
-
+    view.completed(level);
   }
 }
+
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/View.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/View.java
@@ -7,34 +7,41 @@ import com.javarush.task.task34.task3410.model.GameObjects;
 import javax.swing.*;
 
 public class View extends JFrame {
-    private Controller controller;
-    private Field field;
+  private Controller controller;
+  private Field field;
 
-    public View(Controller controller) {
-        this.controller = controller;
-    }
+  public View(Controller controller) {
+    this.controller = controller;
+  }
 
-    public void init() {
-        field = new Field(this);
-        add(field);
+  public void init() {
+    field = new Field(this);
+    add(field);
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(500, 500);
-        setLocationRelativeTo(null);
-        setTitle("Сокобан");
-        setVisible(true);
-    }
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    setSize(500, 500);
+    setLocationRelativeTo(null);
+    setTitle("Сокобан");
+    setVisible(true);
+  }
 
-    public void update() {
-        field.repaint();
-    }
+  public void update() {
+    field.repaint();
+  }
 
-    public GameObjects getGameObjects() {
-        return controller.getGameObjects();
-    }
+  public GameObjects getGameObjects() {
+    return controller.getGameObjects();
+  }
 
-    public void setEventListener(EventListener eventListener) {
-        field.setEventListener(eventListener);
-    }
+  public void setEventListener(EventListener eventListener) {
+    field.setEventListener(eventListener);
+  }
 
+  public void completed(int level) {
+    update();
+    JOptionPane.showMessageDialog(this, "Congratulations!\n" + level + "th level passed.");
+    controller.startNextLevel();
+  }
 }
+
+
