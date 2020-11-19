7cee96cce553e8ccf4d0f45e91f69d45a64d8db8
birdflu <birdflu@mail.ru>
2020-11-19 15:29:13 +0300

    Sokoban (7)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -15,5 +15,6 @@ public class Controller {
 
   public static void main(String[] args) {
     Controller controller = new Controller();
+
   }
 }
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -3,6 +3,7 @@ package com.javarush.task.task34.task3410.view;
 import com.javarush.task.task34.task3410.model.Box;
 import com.javarush.task.task34.task3410.model.Home;
 import com.javarush.task.task34.task3410.model.Player;
+import com.javarush.task.task34.task3410.model.Wall;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,13 +16,9 @@ public class Field extends JPanel {
   }
 
   public void paint(Graphics g) {
-//        Для того чтобы проверить как рисуется твой ящик или игрок, ты можешь создать объект
-//        типа Box или Player в методе paint() класса Field и вызвать у объекта метод draw().
-//        Сделай это исключительно для проверки методов draw(), в дальнейшем метод paint() мы реализуем иначе.
-//
     new Player(35, 35).draw(g);
     new Box(45, 60).draw(g);
     new Home(50, 70).draw(g);
-
+    new Wall(100, 20).draw(g);
   }
 }
